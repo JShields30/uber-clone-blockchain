6429de7af778bcbdcee99a64f88f704e6c2d64f6
Joe Shields <shields.john.joseph@gmail.com>
2022-03-18 13:49:59 -0500

    imported next image and image to be used as temp user image

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'next/image';
+import avatar from '../temp/joeheadshot7-20-20.jpg'
 
 const style = {
 	wrapper: `h-16 w-full bg-black text-white flex md:justify-around items -center px-60 z-20`
@@ -16,7 +18,7 @@ const Navbar = () => {
             <div className={style.rightMenu}>
                 <div className={style.menuItem}>Help</div>
                 <div className={style.menuItem}>Joe</div>
-                <div className={style.userImageContainer}>User Image</div>
+                <div className={style.userImageContainer}></div>
                 </div>
 		</div>
 	);
