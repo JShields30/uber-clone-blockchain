887c0bb705809167b948071103d5416d31cd6191
Joe Shields <shields.john.joseph@gmail.com>
2022-03-18 13:55:09 -0500

    added temp img to layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import avatar from '../temp/joeheadshot7-20-20.jpg'
+import avatar from '../temp/(small)headshot (2).jpg'
 
 const style = {
 	wrapper: `h-16 w-full bg-black text-white flex md:justify-around items -center px-60 z-20`
@@ -18,7 +18,10 @@ const Navbar = () => {
             <div className={style.rightMenu}>
                 <div className={style.menuItem}>Help</div>
                 <div className={style.menuItem}>Joe</div>
-                <div className={style.userImageContainer}></div>
+                <div className={style.userImageContainer}>
+                    <img src={avatar} width={50} height={50} />
+                    </div>
+                    
                 </div>
 		</div>
 	);
