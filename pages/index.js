6ef3b563c6acf302defcd2680705768fa7abd82f
Joe Shields <shields.john.joseph@gmail.com>
2022-03-18 13:08:21 -0500

    created page layout and class names for styling that will be created

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
 }
@@ -6,8 +5,13 @@ const style = {
 export default function Home() {
   return (
     <div className={style.wrapper}>
-      <div>
-
+      {/* navbar */}
+      <div className={style.main}>{/* Map */}</div>
+        <div className={style.rideRequestContainer}>
+          <div className={style.rideRequest}>
+              {/* location select */}
+              {/* confirm ride */}
+          </div>
       </div>
     </div>
   )
