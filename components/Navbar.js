3982b8f4956617150ba577782c670c5840c01941
Joe Shields <shields.john.joseph@gmail.com>
2022-03-18 14:57:13 -0500

    added styling for left menu logo menu item right menu user image container user image and login button components

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
-import avatar from '../temp/(small)headshot (2).jpg'
+import avatar from '../temp/tiny_headshot.jpg'
 
 const style = {
-	wrapper: `h-16 w-full bg-black text-white flex md:justify-around items -center px-60 z-20`
+	wrapper: `h-16 w-full bg-black text-white flex md:justify-around items -center px-60 z-20`,
+    leftMenu: `flex gap-3`,
+    logo: `text-3xl text-white flex cursor-pointer mr-16`,
+    menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor pointer`,
+    rightMenu: `flex gap-3 items-center`,
+    userImageContainer: `mr-2`,
+    userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
+    loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`
+
 };
 
 const Navbar = () => {
@@ -19,7 +27,11 @@ const Navbar = () => {
                 <div className={style.menuItem}>Help</div>
                 <div className={style.menuItem}>Joe</div>
                 <div className={style.userImageContainer}>
-                    <img src={avatar} width={50} height={50} />
+                    <Image 
+                    className={style.userImage} 
+                    src={avatar} 
+                    width={40} 
+                    height={40} />
                     </div>
                     <div>0x00000....00000</div>
                 </div>
