78584a2d21d240adbfa84c7a35cacc0cf0a4e9d8
Joe Shields <shields.john.joseph@gmail.com>
2022-03-18 13:03:06 -0500

    deleted imports

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
@@ -10,7 +7,7 @@ export default function Home() {
   return (
     <div className={style.wrapper}>
       <div>
-        
+
       </div>
     </div>
   )
