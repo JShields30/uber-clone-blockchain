5f02a76ab417e8151c8636c7a11b164c2ecb5a9a
Joe Shields <shields.john.joseph@gmail.com>
2022-03-18 13:02:45 -0500

    created a styled wrapper

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const style = {
+  wrapper: `h-screen w-screen flex flex-col`,
+}
+
 export default function Home() {
   return (
-    <div>
-   Hello
+    <div className={style.wrapper}>
+      <div>
+        
+      </div>
     </div>
   )
 }
