15e3000b49115da3a6f49329ad255cd3353a7d3a
Joe Shields <shields.john.joseph@gmail.com>
2022-03-19 13:04:18 -0500

    imported use effect and created hook to render mapbox

--- components/Map.js
+++ components/Map.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import mapboxgl from 'mapbox-gl'
+import { useEffect } from 'react'
 
 const style = {
     wrapper: `flex-1 h-full w-full`
 }
 
 const Map = () => {
-  return (
-    <div>Map</div>
-  )
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+        
+        })
+
+    }, [])
+
+
+  return <div className={style.wrapper}>Map</div>
 }
 
 export default Map
