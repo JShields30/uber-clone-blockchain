a73d2a7aa060a28d451b2798f6b2f0f885184d11
Joe Shields <shields.john.joseph@gmail.com>
2022-03-19 13:08:22 -0500

    added a container and styles for mapbox

--- components/Map.js
+++ components/Map.js
@@ -9,7 +9,10 @@ const style = {
 const Map = () => {
     useEffect(() => {
         const map = new mapboxgl.Map({
-        
+            container: 'map',
+            style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+            center: [-99.29011, 39.39172],
+            zoom: 3,
         })
 
     }, [])
