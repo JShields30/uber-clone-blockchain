c437b18ed6912a3178ecac76837f757ee789162d
Joe Shields <shields.john.joseph@gmail.com>
2022-03-21 12:16:33 -0500

    added mapboxgl access token

--- components/Map.js
+++ components/Map.js
@@ -6,6 +6,8 @@ const style = {
     wrapper: `flex-1 h-full w-full`
 }
 
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
 const Map = () => {
     useEffect(() => {
         const map = new mapboxgl.Map({
@@ -14,11 +16,10 @@ const Map = () => {
             center: [-99.29011, 39.39172],
             zoom: 3,
         })
-
     }, [])
 
 
-  return <div className={style.wrapper}>Map</div>
+  return <div className={style.wrapper} id ='map' />
 }
 
 export default Map
