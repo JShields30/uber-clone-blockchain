d4d0a72fe2375615ae4b06e7f50859cfb64abeb1
Joe Shields <shields.john.joseph@gmail.com>
2022-03-21 12:26:43 -0500

    created location selector component file and imported usestate

---
+++ components/LocationSelector.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from 'react'
+
+const LocationSelector = () => {
+  return (
+    <div>LocationSelector</div>
+  )
+}
+
+export default LocationSelector
