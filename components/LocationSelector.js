832e1537df1e4ab83be0e1614f84e080a6d70e0d
Joe Shields <shields.john.joseph@gmail.com>
2022-03-21 12:47:12 -0500

    added svg container styled object

--- components/LocationSelector.js
+++ components/LocationSelector.js
@@ -7,6 +7,7 @@ const style = {
     inputBoxes: `flex flex-col mb-4 relative`,
     inputBox: `h-10 mx-4 border-2 bg-[#eeeeee] flex items-center my-1 py-1 px-2`,
     focusedInputBox: `border-black`,
+    svgContainer: `mx-1`,
     input: `my-2 rounded-2 p-2 outline none border-none bg-transparent h-full w-full`,
     verticalLine: `w-0 h-[2rem] border-black border absolute z-10 left-[2.3rem] top-[2rem]`,
 
