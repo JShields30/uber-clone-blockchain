b4bc2b6fef2db8a814cab008c5b64461734338ba
Joe Shields <shields.john.joseph@gmail.com>
2022-03-21 13:06:04 -0500

    added circle svg to inbox box

--- components/LocationSelector.js
+++ components/LocationSelector.js
@@ -29,6 +29,13 @@ const LocationSelector = () => {
                 }`}
                 >
                 <div className={style.svgContainer}>
+                    <svg viewbox='0 0 24 24' width='1em' height='1em'>
+                        <path 
+                            fillRule='evenodd'
+                            clipRule='evenodd'
+                            d='m12 14a2 2 0 100-4 2 2 0 000 4zm5-2a5 5 0 11-10 0 5 5 0 0110 0z'                        
+                        />
+                        </svg>
                     </div>    
                 </div>
             </div>
