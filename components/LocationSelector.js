9685305d3ce5cdf198dadf07b8368c6a25b8d25c
Joe Shields <shields.john.joseph@gmail.com>
2022-03-23 15:47:00 -0500

    created div for svg and added small square svg image

--- components/LocationSelector.js
+++ components/LocationSelector.js
@@ -52,7 +52,17 @@ const LocationSelector = () => {
                     className={`${style.inputBox} ${
                         inFocus === 'to' && style.focusedInputBox
                     }`}
-                ></div>
+                >
+                    <div className={style.svgContainer}>
+                        <svg viewBox='0 0 24 24' width='1em' height='1em'>
+                            <path
+                             fillRule='evenodd'
+                             clipRule='evenodd'
+                             d='M14 10h-4v4h4v-4zM7 7v10h10V7H7z'
+                            />
+                        </svg>
+                    </div>
+                </div>
             </div>
         </div>
   )
